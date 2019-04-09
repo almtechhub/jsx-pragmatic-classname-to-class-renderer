bd88594dbddb44de2d9a7e2b473dc77dd6d3fb92
ALMaclaine <almaclaine@gmail.com>
2019-04-09 11:01:43 -0700

    fix module name

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@
 
 import { getWebpackConfig } from 'grumbler-scripts/config/webpack.config';
 
-const FILE_NAME = 'mylibrary';
-const MODULE_NAME = 'mylibrary';
+const FILE_NAME = 'jsx-pragmatic-classname-to-class-renderer';
+const MODULE_NAME = 'jsx-pragmatic-classname-to-class-renderer';
 
 export const WEBPACK_CONFIG = getWebpackConfig({
     filename:   `${ FILE_NAME }.js`,
